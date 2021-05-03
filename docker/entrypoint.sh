ec8b1d710a29cbc0030db03fcd60cccd0362cdcd
BijoyRayaroth <70229339+BijoyRayaroth@users.noreply.github.com>
2021-05-03 01:51:51 -0400

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,8 +5,6 @@ echo "hello from terraform action"
 git config --global user.email "bijoy.rayaroth@philips.com"
 git config --global user.name "BijoyRayaroth"
 
-git remote add origin https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/$GIT_USERNAME/project.git
-
 git clone https://${GIT_TOKEN}@github.com/BijoyRayaroth/terraform-action.git
 
 cd terraform-action/docker
@@ -14,12 +12,9 @@ cd terraform-action/docker
 terraform init
 echo "Terraform init"
 terraform plan -var="nutanix-password=${NUTANIX_PASSWORD}" -var="nutanix-userName=${INPUT_USERNAME}" -out="terraformPlan"
-echo "apply by ${INPUT_USERNAME}"
-terraform apply -var="nutanix-password=${NUTANIX_PASSWORD}" -var="nutanix-userName=${INPUT_USERNAME}" -auto-approve
+
 
 git add terraformPlan
 git commit -m "adding Plan file"
 echo "Pushing Terraform Plan file"
-git push
-
-
+git push origin master
