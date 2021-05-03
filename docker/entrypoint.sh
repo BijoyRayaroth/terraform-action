39a8cc34aa8e87fead947a5eb31ed7dbef8b913e
BijoyRayaroth <70229339+BijoyRayaroth@users.noreply.github.com>
2021-05-03 00:29:46 -0400

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ echo "hello from terraform action"
 
 git config --global remote.origin.url 'https://${GIT_TOKEN}@github.com/BijoyRayaroth/terraform-action.git'
 
-git clone https://${GIT_TOKEN}@github.com/BijoyRayaroth/terraform-action.git
+git clone https://$GIT_TOKEN@github.com/BijoyRayaroth/terraform-action.git
 
 cd terraform-action/docker
 
